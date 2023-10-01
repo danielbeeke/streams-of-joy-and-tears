9f40f236790056f61bec9ff128ef9317a7411770
Daniel Beeke <mail@danielbeeke.nl>
2023-10-01 12:52:50 +0200

    adding a build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     outDir: 'docs',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        graph: resolve(__dirname, 'graph.html'),
-      },
-    },
   },
   base: ''
 })
